4ae822fc96f6be6d01ed6c0f9927c5f1f5e8e456
工藤貴央 <takaoukudou@macbook.local>
2020-12-28 17:35:37 +0900

    create mewo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-function App() {
-  const greeting = 'Hi!!';
-  const dom = <h1 className="foo">{greeting}</h1>;
-  const input = (
-    <input
-      type="text"
-      onChange={() => {
-        console.log('I am clixked.');
-      }}
-    />
-  );
-  const label = <label htmlFor="bar">bar</label>;
-  return (
-    <React.Fragment>
-      {input}
-      {label}
-    </React.Fragment>
-  );
-}
+// function App() {
+//   const greeting = 'Hi!!';
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//   const input = (
+//     <input
+//       type="text"
+//       onChange={() => {
+//         console.log('I am clixked.');
+//       }}
+//     />
+//   );
+//   const label = <label htmlFor="bar">bar</label>;
+//   return (
+//     <React.Fragment>
+//       {input}
+//       {label}
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+	return (
+		<div>
+			<Cat></Cat>
+		</div>
+	);
+};
+
+const Cat = () => {
+	return <div>Meow!</div>;
+};
 
 export default App;
