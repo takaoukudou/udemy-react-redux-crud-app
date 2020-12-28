8a7b5225d1e5a48725bc086647482c182f2afabc
工藤貴央 <takaoukudou@macbook.local>
2020-12-28 17:20:43 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 
 function App() {
-  const greeting = 'Hi!!'
-  const dom = <h1 className='foo'>{greeting}</h1>
-  const input = <input type="text" onChange={()=> {console.log('I am clixked.')}}/>
-  const label = <label htmlFor='bar'>bar</label>
+  const greeting = 'Hi!!';
+  const dom = <h1 className="foo">{greeting}</h1>;
+  const input = (
+    <input
+      type="text"
+      onChange={() => {
+        console.log('I am clixked.');
+      }}
+    />
+  );
+  const label = <label htmlFor="bar">bar</label>;
   return (
     <React.Fragment>
       {input}
