be853fa443fda36125c010959aaa3850cce72fa1
工藤貴央 <takaoukudou@macbook.local>
2020-12-28 17:08:12 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, {Component}from 'react';
 
 function App() {
-  return <div><h1>Heelo World!</h1></div>;
+  const greeting = 'Hi!!'
+  const dom = <h1 className='foo'>{greeting}</h1>
+  const input = <input type="text" onChange={()=> {console.log('I am clixked.')}}/>
+  const label = <label htmlFor='bar'>bar</label>
+  return (
+    <React.Fragment>
+      {input}
+      {label}
+    </React.Fragment>
+  );
 }
 
 export default App;
