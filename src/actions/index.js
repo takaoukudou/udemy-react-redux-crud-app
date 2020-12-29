ccf31f89bf3e30ac608f9ce3b50fde29bcdc9c80
工藤貴央 <takaoukudou@macbook.local>
2020-12-29 15:23:48 +0900

    add redux

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const INCREMENT = "INCREMENTÇ";
+export const DECREMENT = "DECREMENTÇ";
+export const increment = () => {
+	return {
+		type: INCREMENT,
+	};
+};
+export const decrement = () => {
+	return {
+		type: DECREMENT,
+	};
+};
