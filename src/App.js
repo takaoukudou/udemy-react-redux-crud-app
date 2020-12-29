933eda4e1fa9ec239f5698bb6178853f094bd6c5
工藤貴央 <takaoukudou@macbook.local>
2020-12-29 13:53:37 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from "react";
-
-// function App() {
-//   const greeting = 'Hi!!';
-//   const dom = <h1 className="foo">{greeting}</h1>;
-//   const input = (
-//     <input
-//       type="text"
-//       onChange={() => {
-//         console.log('I am clixked.');
-//       }}
-//     />
-//   );
-//   const label = <label htmlFor="bar">bar</label>;
-//   return (
-//     <React.Fragment>
-//       {input}
-//       {label}
-//     </React.Fragment>
-//   );
-// }
+import PropTypes from "prop-types";
 
 const App = () => {
 	const profiles = [
 		{ name: "Taro", age: 10 },
 		{ name: "Hanako", age: 5 },
-		{ name: "NoName" },
+		{ name: "NoName", age: 3 },
 	];
 	return (
 		<div>
@@ -43,8 +24,9 @@ const User = (props) => {
 	);
 };
 
-User.defaultProps = {
-	age: 1,
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired,
 };
 
 export default App;
